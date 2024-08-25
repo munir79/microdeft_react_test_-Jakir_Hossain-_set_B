3c5206f4dcc5a2be0e357e5f962b5763de1bc61e
munir79 <jakirhossainmunir79@gmail.com>
2024-08-25 16:21:37 +0600

    card

---
+++ src/List/List.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../Card';
+
+const List = () => {
+    const[product,setProduct]=useState([]);
+    useEffect(()=>{
+        fetch('https://react-interview.1putym.easypanel.host/api/product?email=naim.microdeft@gmail.com')
+        .then(res=>res.json())
+        .then(data=>setProduct(data))
+    } ,[])
+    return (
+        <div>
+            {
+                product.map(pr=><Card key={pr.id} pr={pr}></Card>)
+            }
+        </div>
+    );
+};
+
+export default List;
